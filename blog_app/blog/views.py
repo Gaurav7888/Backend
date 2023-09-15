9e9c3996954a28639360627c7bff74a58836a59e
gaurav7888 <sarkargaurav7888@gmail.com>
2023-09-15 23:19:20 +0530

    added few route

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    response = HttpResponse("Hello World")
+    return response
+    
+def html(request):
+
+    developed_by = "gurv"
+    mentors = [
+        "gurav",
+        'srkr'
+    ]
+    context = { 
+        "developer" : developed_by,
+        "mentor" : mentors
+     }
+    response = render(request, 'helloworld.html' , context)
+    return response
